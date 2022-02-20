620eb208d9bf2c565adbefc08f1ffe8689e83652
Kawasaki Hirotsugu <kawa.ikkyou@gmail.com>
2022-02-20 11:44:28 +0900

    第10章完了

--- app/src/main/java/money/Franc.java
+++ app/src/main/java/money/Franc.java
@@ -4,7 +4,4 @@ class Franc extends Money {
     Franc(int amount, String currnecy) {
         super(amount, currnecy);
     }
-    Money times(int multiplier) {
-        return Money.franc(amount * multiplier);
-    }
 }
--- app/src/main/java/money/Money.java
+++ app/src/main/java/money/Money.java
@@ -1,24 +1,29 @@
 package money;
-abstract public class Money {
+public class Money {
     protected int amount;
     protected String currency;
     Money(int amount, String currnecy) {
         this.amount = amount;
         this.currency = currnecy;
     }
-    abstract Money times(int multiplier);
+    Money times(int multiplier) {
+        return new Money(amount * multiplier, currency);
+    };
     String currency() {
         return currency;
     }
     public boolean equals(Object object) {
         Money money = (Money) object;
         return amount == money.amount
-            && getClass().equals(money.getClass());
+            && currency().equals(money.currency());
     }
     static Money dollar(int amount) {
         return new Dollar(amount, "USD");
     }
     static Money franc(int amount) {
         return new Franc(amount, "CHF");
+    }
+    public String toString() {
+        return amount + "" + currency;
     }
 }
